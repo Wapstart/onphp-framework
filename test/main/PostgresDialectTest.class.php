851c5228ff6c3c72e69c66b9d1262aef8ab40048
Alexander A. Zaytsev <a.zaytsev@co.wapstart.ru>
2012-11-30 18:18:49 +0400

    add array quoting tests

--- test/main/PostgresDialectTest.class.php
+++ test/main/PostgresDialectTest.class.php
@@ -11,5 +11,144 @@
 				"'новый год' & 'снегурочка' & 'праздник'"
 			);
 		}
+		
+		public function testQuoteArray()
+		{
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						array(1, 2, 3),
+						array(4, 5, 6)
+					)
+				),
+				'\'{{"1","2","3"},{"4","5","6"}}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						'one """ string with "quotes"'
+					)
+				),
+				'\'{"one \\\\"\\\\"\\\\" string with \\\\"quotes\\\\""}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						'\'single\' quotes \n\n\n'
+					)
+				),
+				'\'{"\\\\\'\'single\\\\\'\' quotes \\\\\\\\n\\\\\\\\n\\\\\\\\n"}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						'"some" string with $var'
+					)
+				),
+				'\'{"\\\\"some\\\\" string with $var"}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array()
+				),
+				'\'{}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						array(''),
+						array('')
+					)
+				),
+				'\'{{""},{""}}\''
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->quoteArray(
+					array(
+						array(1, 2),
+						array(3, 4)
+					),
+					';'
+				),
+				'\'{{"1";"2"};{"3";"4"}}\''
+			);
+		}
+		
+		public function testUnquoteArray()
+		{
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{"string with $var","\\"quoted\\" string"}'
+				),
+				array(
+					'string with $var',
+					'"quoted" string'
+				)
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{{{11,22},{"33",44}},{{55,66},{77,88}}}'
+				),
+				array(
+					array(
+						array('11', '22'),
+						array('33', '44')
+					),
+					array(
+						array('55', '66'),
+						array('77', '88')
+					)
+				)
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{{simpletext,"text with spaces"},{\'quoted\'text,"zlex,zlex"}}'
+				),
+				array(
+					array('simpletext', 'text with spaces'),
+					array('\'quoted\'text', 'zlex,zlex')
+				)
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{{1;2};{3;4}}',
+					';'
+				),
+				array(
+					array(1, 2),
+					array(3, 4)
+				)
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{"",""}'
+				),
+				array('', '')
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					'{}'
+				),
+				array()
+			);
+			
+			$this->assertEquals(
+				PostgresDialect::me()->unquoteArray(
+					''
+				),
+				array()
+			);
+		}
 	}
 ?>
